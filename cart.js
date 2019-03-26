530d09a3eb6bfc690f21f0401ab71a1fd5d8f278
Shacklette95 <46468927+Shacklette95@users.noreply.github.com>
2019-03-25 20:42:14 -0400

    added error message to  cart buttons

---
+++ cart.js
@@ -0,0 +1,14 @@
+
+//cart message
+document.getElementById("btn-cart").onclick = function(){alert("Sorry\nUnfortunately the cart button is not working right now.")};
+
+//Add to cart message
+// Get an array of buttons from the page
+var buttons = document.getElementsByClassName("btn+2cart");
+
+// Loop through the resulting array
+for(var i = 0; i < buttons.length; i++){
+  buttons[i].addEventListener("click", function() {
+    alert("Sorry\nThe add to cart button is not working right now.");
+  });
+}
